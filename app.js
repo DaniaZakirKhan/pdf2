c9e31746c8eeb8f859291d610e0f8e7dce4b850a
Dania Zakir <zakirdania3@gmail.com>
2024-05-10 01:39:01 +0500

    add app.js and index.html

---
+++ app.js
@@ -0,0 +1,89 @@
+// 1.Declare a variable called username.
+//2.Declare a variable called myName & assign to it a string
+//that represents your Full Name.
+
+var username;
+username="Dania Zakir Khan";
+document.write(username+"</br>");
+
+//3. Write script to Declare a JS variable, titled message.
+var message;
+ 
+ 
+//b) Assign “Hello World” to variable message
+message="The text to display in the alert box.";
+document.write(message+"</br>");
+
+
+
+
+//c) Display the message in alert box.
+
+alert("The text to display in the alert box.");
+
+//Write a script to save student’s bio data in JS variables and
+//show the data in alert boxes.
+var firstname="Sophia";
+document.write(firstname+"</br>");
+
+
+
+
+
+var lastname="Doe";
+document.write(lastname+"</br>");
+
+
+
+
+var age="37";
+document.write(age+"</br>");
+
+
+
+
+var country="New York";
+document.write(country+"</br>");
+
+
+
+
+
+alert("FirstName:" + firstname);
+alert("LastName:" + lastname);
+alert("Age:" + age);
+alert("Country:" + country);
+
+//5. Write a script to display the following alert using one JS
+//variable:
+
+
+var message = "PIZZA\nPIZ\nPI\np";
+  alert(message);
+  document.write("<pre>"+message+"</br>"+"</pre>");
+
+  
+
+  //6. Declare a variable called email and assign to it a string that
+//represents your Email Address(e.g. example@example.com).
+//Show the blow mentioned message in an alert box.(Hint: use
+//string concatenation)
+
+var email="My email address is example@example.com"
+alert(email);
+document.write(email+"</br>")
+
+//7.Declare a variable called book & give it the value “A
+//smarter way to learn JavaScript”. Display the following
+//message in an alert box:
+
+var book="I am trying to learn from the book A smarter way to learn javascript";
+alert(book);
+document.write(book+"</br>")
+
+
+//8. Write a script to display this in browser through JS
+
+var following;
+following="Yah!I can write HTML content through javascript";
+document.write(following);
